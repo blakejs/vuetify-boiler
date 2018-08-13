da49c0ffc3a6b5e4258586a7f4e0c067141cc8b8
blake <zuttton@gmail.com>
2018-08-13 11:10:19 -0400

    init repo

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list>
+        <v-list-tile value="true" v-for="(item, i) in items" :key="i">
+          <v-list-tile-action>
+            <v-icon v-html="item.icon"></v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title v-text="item.title"></v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Title</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat>Link One</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <v-content>
+      <router-view/>
+    </v-content>
+    <v-footer app>
+      <span class="pa-2">BlakeJS &copy; 2018</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: false,
+      items: [{
+        icon: 'home',
+        title: 'Home'
+      }],
+      title: 'App'
+    }
+  },
+  name: 'App'
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container fluid>
+    <v-layout column align-center>
+      <blockquote>
+        &#8220;First, solve the problem. Then, write the code.&#8221;
+      </blockquote>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+</script>
+
+
+<style scoped>
+</style>
